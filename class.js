41b0c21b9f0a7dbffcfe139cf9df2a04ff942a74
Oleg Shynkarenko <oleg.shynkarenko1985@gmail.com>
2018-09-24 20:10:41 +0300

    Add pre-commit, prettier and js-beautify

--- class.js
+++ class.js
@@ -1,8 +1,8 @@
 export default class Car {
-    constructor(brand, model, color, topSpeed) {
-        this.brand = brand;
-        this.model = model;
-        this.color = color;
-        this.topSpeed = topSpeed;
-    }
+  constructor(brand, model, color, topSpeed) {
+    this.brand = brand;
+    this.model = model;
+    this.color = color;
+    this.topSpeed = topSpeed;
+  }
 }

28916d5de953381d96623ba6ef174311b3757ea4
Oleg Shynkarenko <oleg.shynkarenko1985@gmail.com>
2018-09-26 20:29:09 +0300

    Added html webpack plugin

--- src/func.js
+++ src/func.js
@@ -1,8 +1,6 @@
-function timeOfRide(obj, location, distance) {
+export function timeOfRide(obj, location, distance) {
   let time = obj.topSpeed / distance;
   console.log(`If you will ride ${obj.topSpeed} kilometers per hour on your 
     ${obj.color} ${obj.brand} ${obj.model} constantly you'll reach 
     ${location} in ${time.toFixed(2)} h.`);
 }
-
-export { timeOfRide };
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./app.js",
+  entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "dist/js"),
     filename: "bundle.js"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      filename: "./../../index.html"
+    })
+  ],
   mode: "development"
 };
